6e6585d4d786e73368ddb2afabec94aa41682c28
DimaEvchuk <74135637+DimaEvchuk@users.noreply.github.com>
2022-05-22 22:27:13 +0300

    В карусели добавил границу картинок,
    и закрытие при картинок

--- src/Galeria/Galeria.jsx
+++ src/Galeria/Galeria.jsx
@@ -4,24 +4,32 @@ import { useState } from "react";
 
 function Galeria() {
   const [indexClickedImg, setIndexClickedImg] = useState(4);
+  let carousel;
+  let carouselImg;
 
   let roadToPicture = [];
+
   for (let i = 1; i <= 14; i++) {
     roadToPicture.push(
       process.env.PUBLIC_URL + "/assets/Galeria-foto/" + i + ".jpg"
     );
   }
 
+  const openCarousel = (idx) => {
+    setIndexClickedImg(idx);
+    if (!carousel || !carouselImg) {
+      carousel = document.querySelector(".carousel-background");
+      carouselImg = document.querySelector(".carousel");
+    }
+
+    carouselImg.classList.toggle("showCarousel");
+    return carousel.classList.toggle("showCarousel");
+  };
+
   const Pictures = roadToPicture.map(function (name, index) {
     return (
       <div key={index} className="grid-box">
-        <img
-          onClick={() => {
-            setIndexClickedImg(index);
-          }}
-          src={name}
-          alt=""
-        />
+        <img onClick={() => openCarousel(index)} src={name} alt="" />
       </div>
     );
   });
@@ -36,10 +44,9 @@ function Galeria() {
         </div>
         <div className="picture-grid">{Pictures}</div>
       </div>
-      <div className="carousel-background">
-        <div className="carousel">
-          <Carousel roadToImg={roadToPicture} indexImg={indexClickedImg} />
-        </div>
+      <div className="carousel-background" onClick={openCarousel}></div>
+      <div className="carousel">
+        <Carousel roadToImg={roadToPicture} indexImg={indexClickedImg} />
       </div>
     </section>
   );
--- src/Galeria/carousel/Carousel.js
+++ src/Galeria/carousel/Carousel.js
@@ -6,39 +6,34 @@ import "./Carousel.css";
 export const Carousel = ({ roadToImg, indexImg }) => {
   //const [pages, setPages] = useState("");
   const [index, setIndex] = useState(indexImg);
- 
-  
-  useEffect(()=>{
-    setIndex(indexImg);
 
-  },[indexImg])
+  useEffect(() => {
+    setIndex(indexImg);
+  }, [indexImg]);
   //const [offset, setOffset] = useState(0);
 
   const handleLeftArrowClick = () => {
-   
-      setIndex(index - 1);
-    
-    
+    setIndex(index - 1);
   };
 
   const handleRigthArrowClick = () => {
-   
-      setIndex(index + 1);
-    
+    setIndex(index + 1);
   };
-
- 
 
   return (
     <div className="main-container">
-      <ArrowBackIosIcon className="arrow" onClick={handleLeftArrowClick} />
-      <div className="window">
-      
-          <img src={roadToImg[index]} alt="" />
-          
-        
+      <div className="arrow-left">
+        <ArrowBackIosIcon className="arrow" onClick={handleLeftArrowClick} />
       </div>
-      <ArrowForwardIosIcon className="arrow" onClick={handleRigthArrowClick} />
+      <div className="window">
+        <img src={roadToImg[index]} alt="" />
+      </div>
+      <div>
+        <ArrowForwardIosIcon
+          className="arrow"
+          onClick={handleRigthArrowClick}
+        />
+      </div>
     </div>
   );
 };
